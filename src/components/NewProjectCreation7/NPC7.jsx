18ebf27f300b20d0f5dbc00644d492a97db9443b
daniels-claimscrafters <daniels@claimscrafters.com>
2024-03-15 12:47:01 -0700

    Update NPC7.jsx

--- src/components/NewProjectCreation7/NPC7.jsx
+++ src/components/NewProjectCreation7/NPC7.jsx
@@ -101,8 +101,8 @@ const NPC7 = ({ updateLoadingState, npcData, onInputChange, onPrevious, numberOf
       if (response.ok) {
         // Payment successful, handle success case
         console.log('Payment successful!');
-        //updateLoadingState(true);
-        //setTimeout(() => {navigate('/pmhs');}, 15000);
+        updateLoadingState(true);
+        setTimeout(() => {navigate('/pmhs');}, 10000);
       } else {
         // Payment failed, handle error case
         const errorData = await response.json();
